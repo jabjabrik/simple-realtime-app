c0dbfa7c152648d844b96736fe459c13fbd9df5c
Jabjabrik <Yusufjabriko>
2023-08-01 12:14:09 +0700

    add

--- app.js
+++ app.js
@@ -23,4 +23,4 @@ const PORT = process.env.PORT || 3000
 // });
 
 // httpServer.listen(PORT);
-app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
